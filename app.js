89b1000c98272d7237368d1b6b59c10a6402ead1
Fernando Banchero <rfbanchero@gmail.com>
2020-07-21 18:32:18 -0300

    Prueba CD

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Peñarol Peñarol!');
+  res.send('Hola mundo!');
 });
 
 app.listen(3000, function () {
